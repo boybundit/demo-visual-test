6ab29d6bb2ebefece27c8fc66793633043c8f305
Bundit Jitkongchuen <bundit.j@lmwn.com>
2024-06-02 10:56:52 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # demo-visual-test
+
+```sh
+npm install
+npx playwright test --update-snapshots
+npx playwright test
+```
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test('example test', async ({ page }) => {
-  await page.goto('https://playwright.dev');
+test('visual test', async ({ page }) => {
+  await page.setViewportSize({ width: 1512, height: 982 });
+  await page.goto('https://bundit.net');
   await expect(page).toHaveScreenshot('landing.png');
 });
